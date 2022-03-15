d6a37f5e77c835a06f9b3ed4ef004f955bfcead3
thakareshubh <shubhamthakare032@gmail.com>
2022-03-15 14:08:50 +0530

    Find maximum by using generic

---
+++ Refactor1_GenericMaxNumber/Refactor1_GenericMaxNumber/FindMax.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refactor1_GenericMaxNumber
+{
+    internal class FindMax<T> where T : IComparable
+    {
+        public T First;
+        public T Second;
+        public T Third;
+
+        public  FindMax(T first,T Second,T Third)
+        {
+            this.First=first;
+            this.Third=Third;
+            this.Second = Second;
+        }
+
+        public void GetMaxNUmber()
+        {
+            if (First.CompareTo(Second) > 0 && First.CompareTo(Third) > 0)
+            {
+                Console.WriteLine($"{First} is maximum");
+            }
+            if (Second.CompareTo(First) > 0 && Second.CompareTo(Third) > 0)
+            {
+                Console.WriteLine($"{Second} is maximum");
+            }
+            if (Third.CompareTo(First) > 0 && Third.CompareTo(Second) > 0) 
+            {
+                Console.WriteLine($"{Third} is maximum");
+            }
+           
+        }
+
+    }
+}
---
+++ Refactor1_GenericMaxNumber/Refactor1_GenericMaxNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Refactor1_GenericMaxNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FindMax<int> max = new FindMax<int>(45, 86, 36);
+            max.GetMaxNUmber();
+            FindMax<double> max2 = new FindMax<double>(1.6, 0045.9, 32.6);
+            max2.GetMaxNUmber();
+            FindMax<string> max3 = new FindMax<string>("456", "asdf", "2");
+            max3.GetMaxNUmber();
+        }
+    }
+}
