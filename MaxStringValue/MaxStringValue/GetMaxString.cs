01e42064fb7c412bc49358513fd76fa6c03b807f
thakareshubh <shubhamthakare032@gmail.com>
2022-03-15 14:00:07 +0530

    Added max string value project

---
+++ MaxStringValue/MaxStringValue/GetMaxString.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxStringValue
+{
+    internal class GetMaxString
+    {
+        public static string MaxString(string first, string second, string third)
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                return first;
+            }
+            if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                return second;
+            }
+            if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
+            {
+                return third;
+            }
+            throw new Exception("Three string value are same");
+        }
+
+    }
+}
---
+++ MaxStringValue/MaxStringValue/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MaxStringValue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find the maximum string value");
+            string stringValue = GetMaxString.MaxString("Alpha", "Beta", "Gama");
+            Console.WriteLine(stringValue);
+        }
+    }
+}
