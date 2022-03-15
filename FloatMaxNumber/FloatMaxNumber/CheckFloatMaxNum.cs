fb3e8e08920a3bd8e1a70b02c2caf3a9a8b999bc
thakareshubh <shubhamthakare032@gmail.com>
2022-03-15 13:54:53 +0530

    creat project to find floating max number

---
+++ FloatMaxNumber/FloatMaxNumber/CheckFloatMaxNum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloatMaxNumber
+{
+    internal class CheckFloatMaxNum
+    {
+        public static double GetFloatMaxNumber(double first, double second, double third)
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                return first;
+            }
+            if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                return second;
+            }
+            if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
+            {
+                return third;
+            }
+            throw new Exception("three numbers are same");
+        }
+
+
+
+    }
+}
---
+++ FloatMaxNumber/FloatMaxNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FloatMaxNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find maximum floating number");
+            double doubleMax = CheckFloatMaxNum.GetFloatMaxNumber(45.23, 63.45, 12.36);
+            Console.WriteLine(doubleMax);
+        }
+    }
+}
