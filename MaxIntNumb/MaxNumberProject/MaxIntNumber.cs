d3446fc5cf7ab6b7165f5ba74942254645c56f7c
thakareshubh <shubhamthakare032@gmail.com>
2022-03-15 13:51:29 +0530

    create project to find max int number

---
+++ MaxIntNumb/MaxNumberProject/MaxIntNumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxNumberProject
+{
+    internal class MaxIntNumber
+    {
+        ///Mehthod is creating for maximum intger number 
+        public static int GetMaxNumber(int first,int second,int third)
+        {
+            if(first.CompareTo(second) > 0 && first.CompareTo(third) >0)
+            {
+                return first;
+            }
+            if (second.CompareTo(first) > 0 && second.CompareTo(third) >0)
+            {
+                return second;
+            }
+            if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
+            {
+                return third;
+            }
+            throw new Exception("three numbers are same");
+        }
+
+      
+    }
+}
---
+++ MaxIntNumb/MaxNumberProject/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MaxNumberProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find maximum number between three integer number");
+            int intMax = MaxIntNumber.GetMaxNumber(5, 4, 45);
+            Console.WriteLine(intMax);
+        }
+    }
+}
